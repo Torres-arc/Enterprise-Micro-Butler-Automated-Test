8d63f802ecbd56793f6de60ea1fe8c4d9a963b04
Torres <781707715@qq.com>
2021-03-06 14:25:18 +0800

    新增public页面，公共搜索方法

---
+++ pages/public_page.py
@@ -0,0 +1,45 @@
+from common.base_page import BasePage
+from Location.client_loc import ClientLoc
+from common.statics import get_config
+from time import sleep
+
+
+class PublicPage(BasePage, ClientLoc):
+    def puclic_search_by_client(self, adder):
+        self.click_element(self.find_Element(self._btn_select_adder))  # 点击添加人输入框，进入组织架构
+        sleep(2)
+        self.send_keys(self.find_Element(self._input_addname), adder)  # 输入添加人姓名
+        sleep(1)
+        self.tap_keyboard('enter')
+        sleep(2)
+        self.click_element(self.find_Element(self._btn_adder))
+        sleep(2)
+        self.click_element(self.find_Element(self._btn_confirm))
+        sleep(2)
+
+    def common_search(self, page_num_loc, target_loc, next_page_loc, msg):
+        pages = int(self.get_element_value(self.find_Elements(page_num_loc)))  # 获取搜索结果页数
+        count = 1
+        while pages > 0 and count <= 3:
+            try:
+                msglist = self.get_elements_values(self.find_Elements(target_loc))  # 搜索目标
+            except TimeoutError:
+                try:
+                    self.check_exist_in_page('暂无数据')  # 无数据时的页面信息
+                except Exception as e:
+                    raise e
+                else:
+                    print('搜索条件下无数据')
+            except Exception as e:
+                raise e
+            else:  # 存在数据，进行验证
+                for i in msglist:
+                    try:
+                        self.check_exist_in_lists(msg, i)
+                    except AssertionError as e:
+                        print('搜索条件与数据不匹配')
+                        raise e
+            self.click_element(self.find_Element(next_page_loc))  # 翻页
+            sleep(2)
+            pages -= 1
+            count += 1
