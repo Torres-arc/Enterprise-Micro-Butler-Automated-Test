886b0d811744d78a18f15aa3164edbbb3b57e008
soyo <876503351@qq.com>
2021-03-09 18:03:39 +0800

    新增脚本：企业群发：模式发送给客户：全部客户，按拉人的指定客户 发送给客户群：按群主查找客户群 5种类型：文本，图片，海报，小程序，网页 一共16条用例

--- test_cases/TestClientMarketing/test_send_enterprise_message.py
+++ test_cases/TestClientMarketing/test_send_enterprise_message.py
@@ -18,6 +18,46 @@ class TestSendEnterpriseMessage(MyTest, LoginPage, SendEnterpriseMessage):
         self.assert_enterprise_message('client', 'text', msg)   # 验证
 
     def test_TestSendEnterpriseMessage_02(self):
+        # 验证员工新建欢迎语，测试发送给客户，发送给全部客户,pic
+        self.login(admin['username'], admin['password'])    # 登录
+        self.switch_to_client_marketing_tab()   # 点击客户营销
+        self.switch_to_enterprise_send_message()      # 点击企业群发
+        self.send_enterprise_message('client', 'all', 'pic', msg)  # 发送给客户，全部客户，pic类型
+        self.assert_enterprise_message('client', 'pic', msg)   # 验证
+
+    def test_TestSendEnterpriseMessage_03(self):
+        # 验证员工新建欢迎语，测试发送给客户，发送给全部客户,poster
+        self.login(admin['username'], admin['password'])    # 登录
+        self.switch_to_client_marketing_tab()   # 点击客户营销
+        self.switch_to_enterprise_send_message()      # 点击企业群发
+        self.send_enterprise_message('client', 'all', 'poster', msg)  # 发送给客户，全部客户，poster类型
+        self.assert_enterprise_message('client', 'poster', msg)   # 验证
+
+    def test_TestSendEnterpriseMessage_04(self):
+        # 验证员工新建欢迎语，测试发送给客户，发送给全部客户,web
+        self.login(admin['username'], admin['password'])    # 登录
+        self.switch_to_client_marketing_tab()   # 点击客户营销
+        self.switch_to_enterprise_send_message()      # 点击企业群发
+        self.send_enterprise_message('client', 'all', 'web', msg)  # 发送给客户，全部客户，web类型
+        self.assert_enterprise_message('client', 'web', msg)   # 验证
+
+    def test_TestSendEnterpriseMessage_05(self):
+        # 验证员工新建欢迎语，测试发送给客户，发送给全部客户,mini
+        self.login(admin['username'], admin['password'])    # 登录
+        self.switch_to_client_marketing_tab()   # 点击客户营销
+        self.switch_to_enterprise_send_message()      # 点击企业群发
+        self.send_enterprise_message('client', 'all', 'mini', msg)  # 发送给客户，全部客户，mini类型
+        self.assert_enterprise_message('client', 'mini', msg)   # 验证
+
+    def test_TestSendEnterpriseMessage_06(self):
+        # 验证员工新建欢迎语，测试发送给客户，按条件筛选客户,text
+        self.login(admin['username'], admin['password'])    # 登录
+        self.switch_to_client_marketing_tab()   # 点击客户营销
+        self.switch_to_enterprise_send_message()      # 点击企业群发
+        self.send_enterprise_message('client', staff, 'text', msg)  # 发送给客户，按staff筛选， text类型
+        self.assert_enterprise_message('client', 'text', msg)    # 验证
+
+    def test_TestSendEnterpriseMessage_07(self):
         # 验证员工新建欢迎语，测试发送给客户，按条件筛选客户,pic
         self.login(admin['username'], admin['password'])    # 登录
         self.switch_to_client_marketing_tab()   # 点击客户营销
@@ -25,7 +65,47 @@ class TestSendEnterpriseMessage(MyTest, LoginPage, SendEnterpriseMessage):
         self.send_enterprise_message('client', staff, 'pic', msg)  # 发送给客户，按staff筛选， pic类型
         self.assert_enterprise_message('client', 'pic', msg)    # 验证
 
-    def test_TestSendEnterpriseMessage_03(self):
+    def test_TestSendEnterpriseMessage_08(self):
+        # 验证员工新建欢迎语，测试发送给客户，按条件筛选客户,poster
+        self.login(admin['username'], admin['password'])    # 登录
+        self.switch_to_client_marketing_tab()   # 点击客户营销
+        self.switch_to_enterprise_send_message()      # 点击企业群发
+        self.send_enterprise_message('client', staff, 'poster', msg)  # 发送给客户，按staff筛选， poster类型
+        self.assert_enterprise_message('client', 'poster', msg)    # 验证
+
+    def test_TestSendEnterpriseMessage_09(self):
+        # 验证员工新建欢迎语，测试发送给客户，按条件筛选客户,web
+        self.login(admin['username'], admin['password'])    # 登录
+        self.switch_to_client_marketing_tab()   # 点击客户营销
+        self.switch_to_enterprise_send_message()      # 点击企业群发
+        self.send_enterprise_message('client', staff, 'web', msg)  # 发送给客户，按staff筛选， web类型
+        self.assert_enterprise_message('client', 'web', msg)    # 验证
+
+    def test_TestSendEnterpriseMessage_10(self):
+        # 验证员工新建欢迎语，测试发送给客户，按条件筛选客户,mini
+        self.login(admin['username'], admin['password'])    # 登录
+        self.switch_to_client_marketing_tab()   # 点击客户营销
+        self.switch_to_enterprise_send_message()      # 点击企业群发
+        self.send_enterprise_message('client', staff, 'mini', msg)  # 发送给客户，按staff筛选， mini类型
+        self.assert_enterprise_message('client', 'mini', msg)    # 验证
+
+    def test_TestSendEnterpriseMessage_11(self):
+        # 验证员工新建欢迎语，测试发送给客户群，按群主选择客户群,text
+        self.login(admin['username'], admin['password'])    # 登录
+        self.switch_to_client_marketing_tab()   # 点击客户营销
+        self.switch_to_enterprise_send_message()      # 点击企业群发
+        self.send_enterprise_message('client_group', staff, 'text', msg)  # 发送给客户群，按群主选择客户群,text
+        self.assert_enterprise_message('client_group', 'text', msg)    # 验证
+
+    def test_TestSendEnterpriseMessage_12(self):
+        # 验证员工新建欢迎语，测试发送给客户群，按群主选择客户群,pic
+        self.login(admin['username'], admin['password'])    # 登录
+        self.switch_to_client_marketing_tab()   # 点击客户营销
+        self.switch_to_enterprise_send_message()      # 点击企业群发
+        self.send_enterprise_message('client_group', staff, 'pic', msg)  # 发送给客户群，按群主选择客户群,pic
+        self.assert_enterprise_message('client_group', 'pic', msg)    # 验证
+
+    def test_TestSendEnterpriseMessage_13(self):
         # 验证员工新建欢迎语，测试发送给客户群，按群主选择客户群,poster
         self.login(admin['username'], admin['password'])    # 登录
         self.switch_to_client_marketing_tab()   # 点击客户营销
@@ -33,15 +113,23 @@ class TestSendEnterpriseMessage(MyTest, LoginPage, SendEnterpriseMessage):
         self.send_enterprise_message('client_group', staff, 'poster', msg)  # 发送给客户群，按群主选择客户群,poster
         self.assert_enterprise_message('client_group', 'poster', msg)    # 验证
 
-    def test_TestSendEnterpriseMessage_04(self):
-        # 验证员工新建欢迎语，测试发送给客户群，按群主选择客户群,poster
+    def test_TestSendEnterpriseMessage_14(self):
+        # 验证员工新建欢迎语，测试发送给客户群，按群主选择客户群,web
         self.login(admin['username'], admin['password'])    # 登录
         self.switch_to_client_marketing_tab()   # 点击客户营销
         self.switch_to_enterprise_send_message()      # 点击企业群发
-        self.send_enterprise_message('client_group', staff, 'mini', msg)  # 发送给客户群，按群主选择客户群,poster
+        self.send_enterprise_message('client_group', staff, 'web', msg)  # 发送给客户群，按群主选择客户群,web
+        self.assert_enterprise_message('client_group', 'web', msg)    # 验证
+
+    def test_TestSendEnterpriseMessage_15(self):
+        # 验证员工新建欢迎语，测试发送给客户群，按群主选择客户群,mini
+        self.login(admin['username'], admin['password'])    # 登录
+        self.switch_to_client_marketing_tab()   # 点击客户营销
+        self.switch_to_enterprise_send_message()      # 点击企业群发
+        self.send_enterprise_message('client_group', staff, 'mini', msg)  # 发送给客户群，按群主选择客户群,mini
         self.assert_enterprise_message('client_group', 'mini', msg)    # 验证
 
-    def test_TestSendEnterpriseMessage_05(self):
+    def test_TestSendEnterpriseMessage_16(self):
         # 验证员工新建欢迎语，测试发送给客户群，按群主选择客户群,web，后搜索
         self.login(admin['username'], admin['password'])    # 登录
         self.switch_to_client_marketing_tab()   # 点击客户营销
@@ -50,15 +138,5 @@ class TestSendEnterpriseMessage(MyTest, LoginPage, SendEnterpriseMessage):
         self.assert_enterprise_message('client_group', 'web', msg)    # 验证
         self.search_enterprise_message('web', msg)
         self.assert_enterprise_message('client_group', 'web', msg)    # 验证
-    #
-    # def test_TestSendEnterpriseMessage_06(self):
-    #     # 验证员工新建欢迎语，测试发送给客户群，按群主选择客户群,web，后搜索
-    #     self.login(admin['username'], admin['password'])    # 登录
-    #     self.switch_to_client_marketing_tab()   # 点击客户营销
-    #     self.switch_to_enterprise_send_message()      # 点击企业群发
-    #     self.send_enterprise_message('client_group', staff, 'web', msg)  # 发送给客户群，按群主选择客户群,poster
-    #     self.assert_enterprise_message('client_group', 'web', msg)    # 验证
-    #     self.search_enterprise_message('web', msg)
-    #     self.assert_enterprise_message('client_group', 'web', msg)    # 验证
 
 
