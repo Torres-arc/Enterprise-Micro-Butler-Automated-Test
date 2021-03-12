bc1dfec587566dd394a74c486552dc6e51392e88
soyo <876503351@qq.com>
2021-03-12 11:00:52 +0800

    完全群裂变

--- test_cases/TestClientMarketing/test_fission_action.py
+++ test_cases/TestClientMarketing/test_fission_action.py
@@ -12,23 +12,13 @@ mission_fission_number = get_config('fission_action', 'mission_fission_number') 
 mission_fission_start_member = get_config('fission_action', 'mission_fission_start_member')     # 任务裂变发起人
 mission_fission_tag = get_config('fission_action', 'mission_fission_tag')       # 任务裂变标签
 mission_fission_gift = get_config('fission_action', 'mission_fission_gift')     # 兑奖码
-
-
-
-# edit_group_sop_name = get_config('sop', 'edit_group_sop_name')  # 客户群的sop名称修改
-# group_sop_limit = get_config('sop', 'group')    # 客户群的sop群组
-# group_submit_name = get_config('sop', 'group_submit_name')  # 客户群的推送名称
-# edit_group_submit_name = get_config('sop', 'edit_group_submit_name')    # 客户群的推送名称修改
-# group_msg = get_config('sop', 'group_msg')  # 客户群推送内容
-# edit_group_msg = get_config('sop', 'edit_group_msg')    # 客户群的推送内容修改
-# # 群裂变
-# friends_circle_sop_name = get_config('sop', 'friends_circle_sop_name')     # 朋友圈的sop名称
-# edit_friends_circle_sop_name = get_config('sop', 'edit_friends_circle_sop_name')    # 朋友圈的sop名称修改
-# staff = get_config('sop', 'staff')  # 朋友圈的职工姓名
-# friends_circle_submit_name = get_config('sop', 'friends_circle_submit_name') # 朋友圈的推送名称
-# edit_friends_circle_submit_name = get_config('sop', 'edit_friends_circle_submit_name') # 朋友圈的推送名称修改
-# friends_circle_msg = get_config('sop', 'friends_circle_msg')    # 朋友圈推送内容
-# edit_friends_circle_msg = get_config('sop', 'edit_friends_circle_msg')  # 朋友圈的推送内容修改
+# 群裂变
+group_fission_name = get_config('fission_action', 'group_fission_name')     # 群裂变名称
+group_fission_guide = get_config('fission_action', 'group_fission_guide')   # 群裂变引导语
+group_fission_number = get_config('fission_action', 'group_fission_number') # 群裂变客户数量
+group_fission_group = get_config('fission_action', 'group_fission_group')   # 群裂变客户群
+group_fission_add_code = get_config('fission_action', 'group_fission_add_code') # 群裂变群活码
+group_fission_gift = get_config('fission_action', 'group_fission_gift') # 群裂变兑奖码
 
 class TestFissionAction(MyTest, LoginPage, FissionActionPage):
     """测试裂变活动"""
@@ -130,3 +120,92 @@ class TestFissionAction(MyTest, LoginPage, FissionActionPage):
         # 活动发起成员：fission_start_member='all'
         # 客户标签：fission_tag=mission_fission_tag
         self.search_fission('mission_fission',mission_fission_name)
+
+    def test_TestFissionAction_06(self):
+        # 验证员工新建群裂变
+        self.login(admin['username'], admin['password'])    # 登录
+        self.switch_to_client_marketing_tab()   # 点击客户营销
+        self.switch_to_fission_action()      # 点击裂变活动
+        self.switch_to_group_fission()      # 点击群裂变
+        # 新建任务裂变
+        self.new_fission('group_fission',group_fission_name, group_fission_guide,group_fission_number, attach_msg,group_fission_gift,'group_helper',
+                         fission_group='all',add_code=group_fission_add_code)
+        # 裂变类型: group_fission
+        # 任务名称：group_fission_name
+        # 裂变引导语：group_fission_guide
+        # 裂变人数：group_fission_number
+        # 裂变海报：attach_msg
+        # 兑奖码：group_fission_gift
+        # 下发途径：group_helper
+        # 选择客户群：fission_group='all'
+        # 群活码：add_code=group_fission_add_code
+        self.switch_to_group_fission()  # 点击群裂变
+        self.assert_fission('group_fission',group_fission_name,group_fission_guide,group_fission_number,group_fission_gift,'group_helper',
+                            fission_group='all')
+
+    def test_TestFissionAction_07(self):
+        # 验证员工新建群裂变
+        self.login(admin['username'], admin['password'])    # 登录
+        self.switch_to_client_marketing_tab()   # 点击客户营销
+        self.switch_to_fission_action()      # 点击裂变活动
+        self.switch_to_group_fission()      # 点击群裂变
+        # 新建任务裂变
+        self.new_fission('group_fission',group_fission_name, group_fission_guide,group_fission_number, attach_msg,group_fission_gift,'group_helper',
+                         fission_group=group_fission_group,add_code=group_fission_add_code)
+        # 裂变类型: group_fission
+        # 任务名称：group_fission_name
+        # 裂变引导语：group_fission_guide
+        # 裂变人数：group_fission_number
+        # 裂变海报：attach_msg
+        # 兑奖码：group_fission_gift
+        # 下发途径：group_helper
+        # 选择客户群：fission_group=group_fission_group
+        # 群活码：add_code=group_fission_add_code
+        self.switch_to_group_fission()  # 点击群裂变
+        self.assert_fission('group_fission',group_fission_name,group_fission_guide,group_fission_number,group_fission_gift,'group_helper',
+                            fission_group=group_fission_group)
+
+    def test_TestFissionAction_08(self):
+        # 验证员工新建群裂变
+        self.login(admin['username'], admin['password'])    # 登录
+        self.switch_to_client_marketing_tab()   # 点击客户营销
+        self.switch_to_fission_action()      # 点击裂变活动
+        self.switch_to_group_fission()      # 点击群裂变
+        # 新建任务裂变
+        self.new_fission('group_fission',group_fission_name, group_fission_guide,group_fission_number, attach_msg,group_fission_gift,'admin_helper',
+                         fission_group=group_fission_group,add_code=group_fission_add_code)
+        # 裂变类型: group_fission
+        # 任务名称：group_fission_name
+        # 裂变引导语：group_fission_guide
+        # 裂变人数：group_fission_number
+        # 裂变海报：attach_msg
+        # 兑奖码：group_fission_gift
+        # 下发途径：admin_helper
+        # 选择客户群：fission_group=group_fission_group
+        # 群活码：add_code=group_fission_add_code
+        self.switch_to_group_fission()  # 点击群裂变
+        self.assert_fission('group_fission',group_fission_name,group_fission_guide,group_fission_number,group_fission_gift,'admin_helper',
+                            fission_group=group_fission_group)
+
+    def test_TestFissionAction_09(self):
+        # 验证员工新建群裂变
+        self.login(admin['username'], admin['password'])    # 登录
+        self.switch_to_client_marketing_tab()   # 点击客户营销
+        self.switch_to_fission_action()      # 点击裂变活动
+        self.switch_to_group_fission()      # 点击群裂变
+        # 新建任务裂变
+        self.new_fission('group_fission',group_fission_name, group_fission_guide,group_fission_number, attach_msg,group_fission_gift,'admin_helper',
+                         fission_group='all',add_code=group_fission_add_code)
+        # 裂变类型: group_fission
+        # 任务名称：group_fission_name
+        # 裂变引导语：group_fission_guide
+        # 裂变人数：group_fission_number
+        # 裂变海报：attach_msg
+        # 兑奖码：group_fission_gift
+        # 下发途径：admin_helper
+        # 选择客户群：fission_group='all'
+        # 群活码：add_code=group_fission_add_code
+        self.switch_to_group_fission()  # 点击群裂变
+        # self.assert_fission('group_fission',group_fission_name,group_fission_guide,group_fission_number,group_fission_gift,'admin_helper',
+        #                     fission_group='all')
+        self.search_fission('group_fission', group_fission_name)
