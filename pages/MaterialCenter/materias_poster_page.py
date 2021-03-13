5df5f5d57166eb21e2d6641f2829342f09aa3c81
Torres <781707715@qq.com>
2021-03-13 16:47:53 +0800

    优化注释

--- pages/MaterialCenter/materias_poster_page.py
+++ pages/MaterialCenter/materias_poster_page.py
@@ -16,7 +16,7 @@ class MaterialPosterPage(BasePage, MaterialPosterLoc):
         self.send_keys(self.find_Element(self._input_poster_name), poster)  # 输入海报名称
         sleep(2)
 
-    def save_poster(self, poster):
+    def save_poster(self):
         # 提交添加海报
         self.click_element(self.find_Element(self._btn_title))  # 关闭分组弹窗
         sleep(2)
@@ -26,13 +26,5 @@ class MaterialPosterPage(BasePage, MaterialPosterLoc):
         self.click_element(self.find_Element(self._btn_save))  # 点击保存
         sleep(2)
 
-    def assert_poster(self, poster):
-        # 验证海报搜索结果
-        name_list = self.get_elements_values(self.find_Elements(self._texts_poster_name))  # 获取海报名称列表
-        for i in name_list:
-            self.check_exist_in_lists(poster, i)  # 验证搜索关键词是否存在于海报名称内
 
-    def assert_group(self, group):
-        # 验证主分类列表
-        group_list = self.get_elements_values(self.find_Elements(self._texts_main_groups))
-        self.check_exist_in_lists(group, group_list)
+
--- test_cases/TestMaterialCenter/test_materials_poster.py
+++ test_cases/TestMaterialCenter/test_materials_poster.py
@@ -10,7 +10,7 @@ from pages.MaterialCenter.public_material_page import PublicMaterialPage
 from common.statics import get_config
 
 admin = get_config('3.1_www')  # 读取注册管理员账号
-mpg = get_config('material_poster_page')
+mpp = get_config('material_poster_page')
 
 
 class TestMaterialPoster(MyTest, MaterialPosterPage, PublicPage, PublicMaterialPage):
@@ -20,10 +20,10 @@ class TestMaterialPoster(MyTest, MaterialPosterPage, PublicPage, PublicMaterialP
         self.switch_to_client_marketing_tab()  # 切换到客户营销
         self.switch_to_current()
 
-        self.create_group(mpg['group'])  # 创建主分类
-        self.create_group(mpg['group2'])
-        self.assert_group(mpg['group'])  # 验证分类存在于主分类列表内
-        self.assert_group(mpg['group2'])
+        self.create_group(mpp['group'])  # 创建主分类
+        self.create_group(mpp['group2'])
+        self.assert_group(mpp['group'])  # 验证分类存在于主分类列表内
+        self.assert_group(mpp['group2'])
 
     def test_TestMaterialPoster_02_addPoster(self):
         # 添加海报
@@ -31,21 +31,21 @@ class TestMaterialPoster(MyTest, MaterialPosterPage, PublicPage, PublicMaterialP
         self.switch_to_client_marketing_tab()  # 切换到客户营销
         self.switch_to_current()
 
-        self.create_poster(mpg['poster'])  # 开始创建海报
+        self.create_poster(mpp['poster'])  # 开始创建海报
         self.material_select_group(self._btn_poster_groups,
-                                   (By.XPATH, self._text_select_move_group.format(mpg['group'])),
-                                   (By.XPATH, self._btn_select_poster_group.format(mpg['group'])))  # 选择海报分组
-        self.save_poster(mpg['poster'])  # 上传图片并保存海报
-        self.check_exist_in_page(mpg['poster'])  # 验证海报是否存在于页面内
+                                   (By.XPATH, self._text_select_move_group.format(mpp['group'])),
+                                   (By.XPATH, self._btn_select_poster_group.format(mpp['group'])))  # 选择海报分组
+        self.save_poster()  # 上传图片并保存海报
+        self.check_exist_in_page(mpp['poster'])  # 验证海报是否存在于页面内
 
     def test_TestMaterialPoster_03_searchPoster(self):
-        # 添加海报
+        # 搜索海报
         self.login(admin['username'], admin['password'])
         self.switch_to_client_marketing_tab()  # 切换到客户营销
         self.switch_to_current()
 
-        self.search_by_input(self._input_poster_search, mpg['poster'], self._btn_search)  # 输入关键词并搜索
-        self.assert_poster(mpg['poster'])  # 验证搜索结果是否包含关键词
+        self.search_by_input(self._input_poster_search, mpp['poster'], self._btn_search)  # 输入关键词并搜索
+        self.assert_materials(mpp['poster'], self._texts_poster_name)  # 验证搜索结果是否包含关键词
 
     def test_TestMaterialPoster_04_moveGroup(self):
         # 将海报移动分组
@@ -53,14 +53,14 @@ class TestMaterialPoster(MyTest, MaterialPosterPage, PublicPage, PublicMaterialP
         self.switch_to_client_marketing_tab()  # 切换到客户营销
         self.switch_to_current()
 
-        self.search_by_input(self._input_poster_search, mpg['poster'], self._btn_search)  # 输入关键词并搜索
+        self.search_by_input(self._input_poster_search, mpp['poster'], self._btn_search)  # 输入关键词并搜索
         self.select_all_and_move_group()  # 选择并点击移动分组
         self.material_select_group(self._btn_input_move_group,
-                                   (By.XPATH, self._text_select_move_group.format(mpg['group2'])),
-                                   (By.XPATH, self._btn_select_move_group.format(mpg['group2'])))  # 选择分组
+                                   (By.XPATH, self._text_select_move_group.format(mpp['group2'])),
+                                   (By.XPATH, self._btn_select_move_group.format(mpp['group2'])))  # 选择分组
         self.move_group_confirm()  # 移动分组确认
-        self.enter_specify_group(mpg['group2'])  # 进入分组
-        self.check_exist_in_page(mpg['poster'])  # 验证页面内是否存在目标海报
+        self.enter_specify_group(mpp['group2'])  # 进入分组
+        self.check_exist_in_page(mpp['poster'])  # 验证页面内是否存在目标海报
 
     def test_TestMaterialPoster_05_editPoster(self):
         # 编辑海报
@@ -68,13 +68,13 @@ class TestMaterialPoster(MyTest, MaterialPosterPage, PublicPage, PublicMaterialP
         self.switch_to_client_marketing_tab()  # 切换到客户营销
         self.switch_to_current()
 
-        self.enter_specify_group(mpg['group2'])  # 进入预定分类
+        self.enter_specify_group(mpp['group2'])  # 进入预定分类
         self.enter_edit_page(self._btn_poster, self._btn_edit_poster)  # 进入海报编辑页
-        self.send_keys(self.find_Element(self._input_poster_name), mpg['poster2'])  # 编辑海报名称
+        self.send_keys(self.find_Element(self._input_poster_name), mpp['poster2'])  # 编辑海报名称
         sleep(2)
         self.click_element(self.find_Element(self._btn_save))  # 保存海报
         sleep(2)
-        self.check_exist_in_page(mpg['poster2'])  # 验证页面是否存在修改后的海报
+        self.check_exist_in_page(mpp['poster2'])  # 验证页面是否存在修改后的海报
 
     def test_TestMaterialPoster_06_deletePoster(self):
         # 删除海报
@@ -82,9 +82,9 @@ class TestMaterialPoster(MyTest, MaterialPosterPage, PublicPage, PublicMaterialP
         self.switch_to_client_marketing_tab()  # 切换到客户营销
         self.switch_to_current()
 
-        self.enter_specify_group(mpg['group2'])  # 进入预定分组
-        self.select_all_and_delete_poster()  # 选择并删除海报
-        self.check_not_exist_in_page(mpg['group2'])  # 验证页面不存在目标海报
+        self.enter_specify_group(mpp['group2'])  # 进入预定分组
+        self.select_all_and_delete_material()  # 选择并删除海报
+        self.check_not_exist_in_page(mpp['group2'])  # 验证页面不存在目标海报
 
     def test_TestMaterialPoster_07_createChildGroup(self):
         # 创建子分类
@@ -92,10 +92,10 @@ class TestMaterialPoster(MyTest, MaterialPosterPage, PublicPage, PublicMaterialP
         self.switch_to_client_marketing_tab()  # 切换到客户营销
         self.switch_to_current()
 
-        self.enter_specify_group(mpg['group2'])  # 进入预定分组
-        self.create_child_group(mpg['child_group'])  # 创建子分组
+        self.enter_specify_group(mpp['group2'])  # 进入预定分组
+        self.create_child_group(mpp['child_group'])  # 创建子分组
         namelist = self.get_elements_values(self.find_Elements(self._texts_child_groups))  # 获取子分组名称列表
-        self.check_exist_in_lists(mpg['child_group'], namelist)  # 验证是否创建成功
+        self.check_exist_in_lists(mpp['child_group'], namelist)  # 验证是否创建成功
 
     def test_TestMaterialPoster_08_deleteChildGroup(self):
         # 删除子分类
@@ -103,14 +103,14 @@ class TestMaterialPoster(MyTest, MaterialPosterPage, PublicPage, PublicMaterialP
         self.switch_to_client_marketing_tab()  # 切换到客户营销
         self.switch_to_current()
 
-        self.enter_specify_group(mpg['group2'])  # 进入预定分组
-        self.delete_group(mpg['child_group'])  # 删除子分组
+        self.enter_specify_group(mpp['group2'])  # 进入预定分组
+        self.delete_group(mpp['child_group'])  # 删除子分组
 
         # 验证目标子分组是否还存在
         try:
             self.find_Elements(self._texts_child_groups)
         except TimeoutException:
-            self.check_not_exist_in_page(mpg['child_group'])
+            self.check_not_exist_in_page(mpp['child_group'])
         except Exception as e:
             raise e
 
@@ -120,8 +120,8 @@ class TestMaterialPoster(MyTest, MaterialPosterPage, PublicPage, PublicMaterialP
         self.switch_to_client_marketing_tab()  # 切换到客户营销
         self.switch_to_current()
 
-        self.delete_group(mpg['group'])  # 删除分组
-        self.check_not_exist_in_page(mpg['group'])  # 验证分组是否存在于页面内
-        self.delete_group(mpg['group2'])
-        self.check_not_exist_in_page(mpg['group2'])
+        self.delete_group(mpp['group'])  # 删除分组
+        self.check_not_exist_in_page(mpp['group'])  # 验证分组是否存在于页面内
+        self.delete_group(mpp['group2'])
+        self.check_not_exist_in_page(mpp['group2'])
 
